17b70dc3bab52b6796d1556ec32aa6279d4c7a7f
harshita <harshita1225@gmail.com>
2022-10-11 14:09:15 +0200

    task completed

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ import ReactDOM from "react-dom";
 const Alert = () => {
   const colorClasses = {
     color: "red",
-	text4Red:"OMG! Something really bad has happended!",
-	text4Orange:"Well, it is not that bad after all!"
+    text4Red: "OMG! Something really bad has happended!",
+    text4Orange: "Well, it is not that bad after all!",
   };
-  //your component here
-
+  if (colorClasses.color === "red") {
+    return (
+      <div className="alert alert-danger" role="alert">
+        OMG! Something really bad has happended!
+      </div>
+    );
+  } else {
+    return (
+      <div className="alert alert-warning" role="alert">
+        Well, it is not that bad after all!
+      </div>
+    );
+  }
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understand what properties are being passed to the component
-ReactDOM.render(
-  <Alert/>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Alert />, document.getElementById("root"));
